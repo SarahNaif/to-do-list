713ad73add9c625dfed0b61dfff26859740c741b
SarahNaif <sarah.althowebi@gmail.com>
2024-10-27 12:54:13 +0300

    [feat]: add vaildation for empty input using zod

--- src/components/Form/TaskForm.tsx
+++ src/components/Form/TaskForm.tsx
@@ -1,27 +1,52 @@
 import React, { FormEvent, useState } from 'react'
 import useStore from '../../store/store'
+import {z} from 'zod'
+import { MdError } from "react-icons/md";
+
+
 
 const TaskForm: React.FC = () => {
   const [title, setTitle] = useState<string>("")
+  const [error , setError] = useState<string>("")
   const addTask = useStore((state) => state.addTask)
 
+  const todoSchema = z.object({
+    task: z.string().min(1, { message: 'Task can not be empty. Please add a task.' }), 
+  });
+  
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
-    if (!title.trim()) return;
-    addTask(title)
-    setTitle('')
+
+    try {
+      todoSchema.parse({task: title.trim()})
+      addTask(title.trim())
+      setTitle('')
+      setError('')
+    } catch (err) {
+      if (err instanceof z.ZodError) {
+        setError(err.errors[0].message);
+      }
+    }
+   
 
   }
 
   return (
     <form onSubmit={handleSubmit}>
+      <div className="px-2 rounded-md ">
       <input
         type="text"
         value={title}
-        onChange={(e)=> setTitle(e.target.value)}
-        className="p-3 mt-2 mb-4 w-full bg-slate-200 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none placeholder:italic"
+        onChange={(e)=> {setTitle(e.target.value); setError(error ? '' : error)}}
+         className={`p-3 mt-2 mb-0.5 w-full bg-slate-200 rounded border-2 ${error ? 'border-red-500 bg-red-100/50 focus:border-red-500 ' : 'border-slate-100 bg-slate-100 focus:border-slate-400'}  focus:border-slate-600 focus:outline-none placeholder:italic`}
         placeholder="Add Your Task..."
-      />
+        />
+        {error &&
+          <div className="flex items-center gap-1 text-red-500 text-sm font-semibold px-1"><MdError className="mt-0.5" />{error}
+          </div>
+        }
+      </div>
+     
     </form>
   )
 }
